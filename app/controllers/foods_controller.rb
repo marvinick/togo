54ed4f54adcf8240c0340a3151d8be51047de7dc
marvinick <marviluw@gmail.com>
2015-07-15 08:30:13 -0700

    basic MVC added

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,46 @@
+class FoodsController < ApplicationController
+  before_action :set_food, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @food.update
+      redirect_to food_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @food.destroy
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name)
+  end
+end
---
+++ app/models/food.rb
@@ -0,0 +1,3 @@
+class Food < ActiveRecord::Base
+  validates_presence_of :name
+end
