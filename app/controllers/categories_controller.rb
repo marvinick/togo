bc484c4c057c30d0dee4e84caf99d1aa8e1b8e7c
marvinick <marviluw@gmail.com>
2015-07-15 18:07:10 -0700

    added mvc category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,46 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @category.update(category_params)
+      redirect_to category_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
