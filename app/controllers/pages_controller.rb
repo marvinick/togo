ff23f8c91ce7fa88e46b25862522fa4612ce76a9
marvinick <marviluw@gmail.com>
2015-07-17 13:34:22 -0700

    added instagram api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+     @instagram = Instagram.user_recent_media("415164868", {:count => 1})
   end
 end
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,8 +1,8 @@
 require "instagram"
 Instagram.configure do |config|
 
-config.client_id = "client_id goes here"
+config.client_id =  "ff0c6fe74c57494eb1da956dbdac6bdc"
 
-config.access_token = "access_token goes here"
+config.access_token = "415164868.ff0c6fe.51602b93f35741febf8511f37328517b"
 
 end
