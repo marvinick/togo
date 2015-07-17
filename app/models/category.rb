9909fde41aa67885f7e1dd69a90d014d6ed0b386
marvinick <marviluw@gmail.com>
2015-07-17 10:08:09 -0700

    added gem install instagram

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
   validates_presence_of :name
   has_many :food_categories
-  has_many :categories, through: :food_categories
+  has_many :foods, through: :food_categories
 end
