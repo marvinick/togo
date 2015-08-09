7e9972608a097d88ab380774581d9408b65d4da3
marvinick <marviluw@gmail.com>
2015-08-09 08:59:24 -0700

    it will adjust navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,15 @@ Rails.application.routes.draw do
   resources :foods
   resources :categories
 
-  root to: "pages#home"
+  #root to: "users#login"
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
