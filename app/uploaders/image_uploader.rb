cdbc60a6f465958157a79d2341e98be35adaf3e9
marvinick <marviluw@gmail.com>
2015-08-20 20:56:10 -0700

    images display on foods path and click on image_tag

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_limit => [200, 250]
+     process :resize_to_limit => [200, 300]
    end
 
    version :show do
